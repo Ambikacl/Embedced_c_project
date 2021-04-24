60916871586ade28bf9d241f6b20219b2c0b43ca
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-24 14:08:35 +0530

    Add files via upload

---
+++ inc/files.h
@@ -0,0 +1,17 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+/**
+ * @file files.h
+ * @author Ambika C L
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#endif /* __LIB_H__ */
