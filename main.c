0da53845879499c5eb22a5bdd9223e3f65fe6858
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-24 14:13:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+    // Insert code
+     DDRB|=(1<<PB0); //set B0=1 for LED
+     DDRD&=~(2<<PD0);//Clear bit
+     DDRD&=~(4<<PD0);//Clear bit
+     PORTD|=(2<<PD0);//set bit
+     PORTD|=(4<<PD0);
+
+       while(1)
+    {
+        if(!(PIND&(2<<PD0))&& !(PIND&(4<<PD0)))
+        {
+            PORTB|=(1<<PB0);
+            _delay_ms(2000);
+        }
+        else
+        {
+            PORTB&=~(1<<PB0);
+            _delay_ms(2000);
+        }
+    }
+
+
+    return 0;
+}
