45c3c7a9319c191c07414a6b9695ad244d2bb6b1
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-28 12:26:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,21 +5,21 @@ int main(void)
 
     // Insert code
      DDRB|=(1<<PB0); //set B0=1 for LED
-     DDRD&=~(2<<PD0);//Clear bit
-     DDRD&=~(4<<PD0);//Clear bit
+     DDRD&=~(2<<PD0);//Configuring as input
+     DDRD&=~(4<<PD0);//Configuring as input
      PORTD|=(2<<PD0);//set bit
      PORTD|=(4<<PD0);
 
        while(1)
     {
-        if(!(PIND&(2<<PD0))&& !(PIND&(4<<PD0)))
+        if(!(PIND&(2<<PD0))&& !(PIND&(4<<PD0)))//if person is seated and heater on(i.e.,when both the switches are pressed)
         {
-            PORTB|=(1<<PB0);
+            PORTB|=(1<<PB0);//LED ON
             _delay_ms(2000);
         }
         else
         {
-            PORTB&=~(1<<PB0);
+            PORTB&=~(1<<PB0);//LED OFF
             _delay_ms(2000);
         }
     }
