9f0366e91397007e058c7ade351d78bd053d25cf
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 18:14:18 +0530

    Update act4.h

--- inc/act4.h
+++ inc/act4.h
@@ -2,15 +2,8 @@
 #define ACT4_H_INCLUDED
 
 
-#ifndef UART_H_INCLUDED
-#define UART_H_INCLUDED
-
-
 void USARTInit(uint16_t);
 char USARTReadChar();
 void USARTWriteChar(char);
-
-
-#endif // UART_H_INCLUDED
 
 #endif // ACT4_H_INCLUDED
