f6fad450710dbcfc219d6091fd5f28b57725d17f
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 23:27:44 +0530

    Update act4.h

--- inc/act4.h
+++ inc/act4.h
@@ -1,20 +1,10 @@
 #ifndef ACT4_H_INCLUDED
 #define ACT4_H_INCLUDED
 
-/**
- * @file act4.h
- * @author Ambika C L (259806)
- * @brief states of LED
- * @version 0.1
- * @date 2021-04-29
- *
- * @copyright Copyright (c) 2021
- *
- */
-
-/*USART initialization*/
-void USARTInit(uint16_t);
+void InitUSART(uint16_t);
 char USARTReadChar();
 void USARTWriteChar(char);
+void data_display(uint16_t);
+
 
 #endif // ACT4_H_INCLUDED
