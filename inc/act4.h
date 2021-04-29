8de9e85feb3ccaee1865d329841846551dd1689c
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 18:51:40 +0530

    Update act4.h

--- inc/act4.h
+++ inc/act4.h
@@ -1,6 +1,17 @@
 #ifndef ACT4_H_INCLUDED
 #define ACT4_H_INCLUDED
 
+/**
+ * @file act4.h
+ * @author Ambika C L (259806)
+ * @brief states of LED
+ * @version 0.1
+ * @date 2021-04-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 
 void USARTInit(uint16_t);
 char USARTReadChar();
