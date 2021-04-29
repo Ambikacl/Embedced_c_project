cdedbc9a155df5dce1fd852ffe23af0766fe7ce4
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 23:33:13 +0530

    Update act4.h

--- inc/act4.h
+++ inc/act4.h
@@ -1,9 +1,24 @@
 #ifndef ACT4_H_INCLUDED
 #define ACT4_H_INCLUDED
 
+/**
+ * @file act4.h
+ * @author Ambika C L(259806)
+ * @brief Activity_1
+ * @version 0.1
+ * @date 2021-04-24
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 void InitUSART(uint16_t);
 char USARTReadChar();
 void USARTWriteChar(char);
+/** \brief to send the USART the modified temperature value and dispkay it on the serial monitor
+ *
+ * \param temp_value
+ */
 void data_display(uint16_t);
 
 
