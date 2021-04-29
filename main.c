bbe1b8206f58202e99e711009fc9d3836a96e38a
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 23:27:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,13 +8,43 @@
  * @copyright Copyright (c) 2021
  *
  */
+#include <avr/io.h>
+#include <util/delay.h>
 #include "act1.h"
 #include "act2.h"
+#include "act3.h"
+#include "act4.h"
 
 int main(void)
 {
-    Init_Ports();/* Initialize Ports */
-/* if and only if both button sensor switch and heater switch is on(pressed) LED turns ON */
-	Led_Status();
+    Init_Ports();//Initialize ports for led blinking
+    InitADC();//Initialize ADC ports
+    uint16_t temp=0; //declare temp value to 0
+    setup_PWM();//start PWM generation
+    InitUSART(103);//Initialize ports for USART
+    led_State(LED_OFF);
+
+    while(1)
+          {
+              if(PersonSeated && HeaterOn)//checks whether button and Heater is ON or OFF
+              {
+                    led_State(LED_ON);//blinking of the led
+                    _delay_ms(20);
+
+                    temp= ReadADC(0);//reading the adjusted temp value
+                    _delay_ms(200);
+
+                    pwmout(temp);
+                    data_display(temp);
+
+              }
+               else
+                {
+                    led_State(LED_OFF);
+                    _delay_ms(200);
+                     OCR1A = 0; //PWM wave 0
+                }
+          }
+
     return 0;
 }
