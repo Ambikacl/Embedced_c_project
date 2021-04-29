b512430f78fc922cc22ba16fe34ab04ac6ccac32
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 23:24:21 +0530

    Update act1.c

--- src/act1.c
+++ src/act1.c
@@ -1,14 +1,10 @@
-// include files
+#include <avr/io.h>
 #include "act1.h"
-#include "act2.h"
-#include "act3.h"
-#include "act4.h"
 /**
  * @brief Change the state of the LED Pin according to the value of state
  *
  * @param state Pin level to which the LED Pin should be set
  */
-
 void led_State(uint8_t state)
 {
 	LED_PORT = (state << LED_PIN);
@@ -32,55 +28,4 @@ void Init_Ports(void)
 	SET_PD1_AND_PD2; //for input
         Button_On();
         Heater_On();
-}
-
-void Led_Status(void)
-{
-    uint16_t temp, i;
-    InitADC();
-    setup_PWM();
-    led_State(LED_OFF);
-    while(1){
-        /*checks whether button and Heater is ON or OFF */
-        if(PersonSeated && HeaterOn)
-        {
-            led_State(LED_ON);
-            InitUSART(103);
-            temp=ReadADC(0);
-            if(temp<=200)
-            {
-                PWM_TempValue=51;
-                char temp_value[33] = "Temperature = 20 deg Celsius\n";
-                for(i=0;i<33;i++){USARTWriteChar(temp_value[i]);}
-               _delay_ms(200);
-            }
-            else if(temp<=500 && temp>=210)
-            {
-                PWM_TempValue=102;
-                char temp_value[33] = "Temperature = 25 deg Celsius\n";
-                for(i=0;i<33;i++){USARTWriteChar(temp_value[i]);}
-                _delay_ms(200);
-            }
-            else if(temp<=700 && temp>=510)
-            {
-                PWM_TempValue=179;
-                char temp_value[33] ="Temperature = 29 deg Celsius\n";
-                for(i=0;i<33;i++){USARTWriteChar(temp_value[i]);}
-                _delay_ms(200);
-            }
-            else if(temp<=1024 && temp>=710)
-            {
-                PWM_TempValue=242;
-                char temp_value[33] = "Temperature = 33 deg Celsius\n";
-                for(i=0;i<33;i++){USARTWriteChar(temp_value[i]);}
-                _delay_ms(200);
-            }
-
-        }
-        else
-        {
-            led_State(LED_OFF);
-            _delay_ms(200);
-        }
-}
 }
