132eb5e7ec6abcb84d1175904a9c3d0ed1f73964
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 12:23:47 +0530

    Update act2.h

--- inc/act2.h
+++ inc/act2.h
@@ -1,11 +1,6 @@
 #ifndef ACT2_H_INCLUDED
 #define ACT2_H_INCLUDED
-#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
 
-#define PersonSeated !(PIND&(2<<PD0)) /**< Switch-1 pressed**/
-#define HeaterOn !(PIND&(4<<PD0))/**< Switch-2 pressed**/
-#define LED_ON PORTB|=(1<<PB0)		/**< LED state HIGH */
-#define LED_OFF PORTB&=~(1<<PB0)			/**< LED state LOW */
 /**
  * @file act2.h
  * @author Ambika C L (259806)
