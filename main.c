6e784cd66fbfefd318a946f6349d7dd078bcba71
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-29 12:14:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,20 @@
-#include "files.h"
+/**
+ * @file main.c
+ * @author Ambika C L (259806);
+ * @brief Activity_1
+ * @version 0.1
+ * @date 2021-04-24
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include "act1.h"
+#include "act2.h"
 
 int main(void)
 {
-
-    // Insert code
-     DDRB|=(1<<PB0); //set B0=1 for LED
-     DDRD&=~(2<<PD0);//Configuring as input
-     DDRD&=~(4<<PD0);//Configuring as input
-     PORTD|=(2<<PD0);//set bit
-     PORTD|=(4<<PD0);
-
-       while(1)
-    {
-        if(!(PIND&(2<<PD0))&& !(PIND&(4<<PD0)))//if person is seated and heater on(i.e.,when both the switches are pressed)
-        {
-            PORTB|=(1<<PB0);//LED ON
-            _delay_ms(2000);
-        }
-        else
-        {
-            PORTB&=~(1<<PB0);//LED OFF
-            _delay_ms(2000);
-        }
-    }
-
-
+    Init_Ports();/* Initialize Ports */
+	/* if and only if both button sensor and heater switch is on(pressed) LED turns ON */
+	Led_Status();
     return 0;
 }
