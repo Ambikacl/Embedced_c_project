0d13fd43e01fe22cd681a25f8d1e8c2ae2e1ed7c
Ambika C L <80435833+Ambikacl@users.noreply.github.com>
2021-04-30 09:24:09 +0530

    Update act3.c

--- src/act3.c
+++ src/act3.c
@@ -1,7 +1,5 @@
-#include <avr/io.h>
 #include "act1.h"
 #include "act3.h"
-#include<util/delay.h>
 void setup_PWM(void){
     TCCR1A=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);//Timer_1
     TCCR1B=(1<<WGM12)|(1<<CS11)|(1<<CS10);//64 prescale
